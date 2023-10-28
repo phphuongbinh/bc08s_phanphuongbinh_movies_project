b8192147907404e8dfe1f4a895af0192b453416e
ErauQsPhan <mrsplol102@gmail.com>
2023-10-28 23:08:00 +0700

    reponsive DetailMovie

--- src/pages/DetailMovie/DetailMovie.js
+++ src/pages/DetailMovie/DetailMovie.js
@@ -38,7 +38,7 @@ const DetailMovie = () => {
         />
 
         <div className="overlay absolute top-0 left-0 z-10 w-full h-full inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.7)] via-[rgba(0,0,0,0.3] to-[transparent]"></div>
-        <div className="absolute z-10 text-white bottom-5 left-20 right-20 page-container">
+        <div className="absolute z-10 text-white bottom-5 left-4 right-4 lg:left-20 lg:right-20 page-container">
           <div className="">
             <h3 className="mx-0 mt-3 mb-5 text-3xl font-semibold">
               {movie.tenPhim}
@@ -50,16 +50,16 @@ const DetailMovie = () => {
             </div>
           </div>
 
-          <div className="grid grid-cols-5 gap-4 banner-list">
+          <div className="relative grid grid-cols-2 gap-2 lg:gap-4 sm:grid-cols-3 xl:grid-cols-5 banner-list">
             <div
               onClick={() => setIsOpenTrailer(true)}
-              className="h-[150px] rounded-2xl bg-primary flex justify-center items-center cursor-pointer"
+              className="  h-[60px] hover:scale-105 duration-300 lg:h-[150px] rounded-2xl bg-primary flex justify-center items-center cursor-pointer relative"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-14 h-14"
+                className="w-8 h-8 lg:w-14 lg:h-14"
               >
                 <path
                   fillRule="evenodd"
@@ -68,8 +68,8 @@ const DetailMovie = () => {
                 />
               </svg>
             </div>
-            <div className="h-[150px] gap-3 rounded-2xl bg-primary flex justify-center items-center cursor-pointer">
-              <span className="text-2xl">Đặt vé ngay</span>
+            <div className=" h-[60px] hover:scale-105 duration-300 lg:h-[150px] gap-3 rounded-2xl bg-primary flex justify-center items-center cursor-pointer relative">
+              <span className="text-md lg:text-2xl">Đặt vé ngay</span>
               <span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -77,7 +77,7 @@ const DetailMovie = () => {
                   viewBox="0 0 24 24"
                   strokeWidth="1.5"
                   stroke="currentColor"
-                  className="w-8 h-8"
+                  className="w-5 h-5 lg:w-8 lg:h-8"
                 >
                   <path
                     strokeLinecap="round"
@@ -87,46 +87,40 @@ const DetailMovie = () => {
                 </svg>
               </span>
             </div>
-            <div className="h-[150px] rounded-2xl bg-black/20 backdrop-filter backdrop-blur-md flex relative">
-              <p className="absolute font-semibold text-gray-500 -rotate-90 -translate-x-1/2 -translate-y-1/2 left-5 top-1/2">
+            <div className="col-span-2 sm:col-span-1 h-[60px]  lg:h-[150px] rounded-2xl bg-black/20 backdrop-filter backdrop-blur-md flex relative">
+              <p className="p-2 text-xs font-semibold text-gray-500 lg:text-xl lg:absolute lg:-translate-x-1/2 lg:-translate-y-1/2 lg:-rotate-90 left-5 lg:top-1/2">
                 Thời lượng
               </p>
-              <p className="absolute text-2xl font-bold -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+              <p className="absolute font-bold -translate-x-1/2 lg:-translate-y-1/2 text-md lg:text-2xl top-1/2 left-1/2">
                 126 phút
               </p>
             </div>
-            <div className="h-[150px] rounded-2xl bg-black/20 backdrop-filter backdrop-blur-md flex relative">
-              <p className="absolute font-semibold text-gray-500 -rotate-90 -translate-x-1/2 -translate-y-1/2 left-5 top-1/2">
+            <div className="h-[60px]  lg:h-[150px] rounded-2xl bg-black/20 backdrop-filter backdrop-blur-md flex relative">
+              <p className="p-2 text-xs font-semibold text-gray-500 lg:text-xl lg:absolute lg:-translate-x-1/2 lg:-translate-y-1/2 lg:-rotate-90 left-5 lg:top-1/2">
                 Phát sóng
               </p>
-              <p className="absolute text-2xl font-bold -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+              <p className="absolute font-bold -translate-x-1/2 lg:-translate-y-1/2 text-md lg:text-2xl top-1/2 left-1/2">
                 {moment(movie.ngayKhoiChieu).format("L")}
               </p>
             </div>
-            <div className="h-[150px] rounded-2xl bg-black/20 backdrop-filter backdrop-blur-md flex relative">
-              <p className="absolute font-semibold text-gray-500 -rotate-90 -translate-x-1/2 -translate-y-1/2 left-5 top-1/2">
+            <div className="h-[60px]  lg:h-[150px] rounded-2xl bg-black/20 backdrop-filter backdrop-blur-md flex relative">
+              <p className="p-2 text-xs font-semibold text-gray-500 lg:text-xl lg:absolute lg:-translate-x-1/2 lg:-translate-y-1/2 lg:-rotate-90 left-5 lg:top-1/2">
                 Đánh giá
               </p>
-              <p className="absolute text-2xl font-bold -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+              <p className="absolute font-bold -translate-x-1/2 lg:-translate-y-1/2 text-md lg:text-2xl top-1/2 left-1/2">
                 8/10
               </p>
             </div>
           </div>
         </div>
       </div>
-      <div className="text-gray-300 page-container">
+      <div className="text-gray-300 page-container max-w-[90%]">
         <div className="mb-8">
           <h3 className="mb-2 text-xl font-semibold text-gray-600 uppercase">
             Mô tả
           </h3>
           <p>{movie.moTa}</p>
         </div>
-        {/* <div className="mb-8">
-          <h3 className="-mb-12 text-xl font-semibold text-gray-600 uppercase">
-            Phim khác
-          </h3>
-          <MovieList />
-        </div> */}
       </div>
     </div>
   );
